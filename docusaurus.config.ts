569e1010749628e124ed58b7db061bdbd9e31a2b
Hannes Rudolph <hrudolph@gmail.com>
2025-07-28 15:01:15 -0600

    Revert "Fix mobile search bar visibility and suppress search plugin warnings …" (#279)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -73,7 +73,7 @@ const config: Config = {
       {
         hashed: true,
         language: ["en"],
-        highlightSearchTermsOnTargetPage: false,
+        highlightSearchTermsOnTargetPage: true,
         explicitSearchResultPath: true,
         docsRouteBasePath: "/",
       },
--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import OriginalLayout from '@theme-original/Layout';
 import { AnimatedBackground } from '../../components/AnimatedBackground';
-import { useSearchKeyboardShortcut } from '../../components/SearchKeyboardShortcut';
 
 export default function Layout(props) {
-  useSearchKeyboardShortcut();
-  
   return (
     <>
       <AnimatedBackground />
